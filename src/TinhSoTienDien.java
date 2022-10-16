6570a422a30529eb406e3602ffee2c67a5f5a0ec
lemanhlinh88 <63782267+lemanhlinh88@users.noreply.github.com>
2022-10-16 19:17:19 +0700

    Update TinhSoTienDien.java
    Chỉnh sửa lại hàm tính tong so tien

--- src/TinhSoTienDien.java
+++ src/TinhSoTienDien.java
@@ -8,18 +8,19 @@ public class TinhSoTienDien {
     public int soTienDien(int sodien) {
         int tongsotien = 0;
 
-        int tienmuc1 = sodien > 50 ? 50 * 1678 : sodien * 1678;
-        tongsotien += tienmuc1 > 0 ? tienmuc1 : 0;
-        int tienmuc2= sodien > 100 ? 50 * 1734 : (sodien - 50) * 1734;
-        tongsotien += tienmuc2 > 0 ? tienmuc2 : 0;
-        int tienmuc3 = sodien > 200 ? 100 * 2014 : (sodien - 100) * 2014;
-        tongsotien += tienmuc3 > 0 ? tienmuc3 : 0;
-        int tienmuc4 = sodien > 300 ? 100 * 2536 : (sodien - 200) * 2536;
-        tongsotien += tienmuc4 > 0 ? tienmuc4 : 0;
-        int tienmuc5 = sodien > 400 ? 100 * 2834 : (sodien - 300) * 2834;
-        tongsotien += tienmuc5 > 0 ? tienmuc5 : 0;
-        int tienmuc6 = sodien > 400 ? (sodien - 400) * 2927 : 0;
-        tongsotien += tienmuc6 > 0 ? tienmuc6 : 0;
+        if(sodien < 50)
+            return sodien * 1678;
+        if(sodien < 100)
+            return 50 * 1678 + (sodien - 50) * 1734;
+        if(sodien < 200)
+            return 50 * 1678 + 50 * 1734 + (sodien - 100) * 2014;
+        if(sodien < 300)
+            return 50 * 1678 + 50 * 1734 + 100 * 2014 + (sodien - 200) * 2536;
+        if(sodien < 400)
+            return 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (sodien - 300) * 2834;
+        if(sodien < 400)
+            return 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + 100 * 2834 + (sodien - 400) * 2927;
+
         return tongsotien;
     }
 }
